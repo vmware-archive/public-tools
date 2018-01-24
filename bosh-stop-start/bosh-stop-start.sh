893a8b66b1232a655480c364aa2ef652969b5aae
Alan McGinlay <amcginlay@pivotal.io>
2018-01-24 22:15:31 +0200

    this works for stop functionality (ooops 4).  expand to support stop/start arguments

--- bosh-stop-start/bosh-stop-start.sh
+++ bosh-stop-start/bosh-stop-start.sh
@@ -4,22 +4,19 @@ function printline() {
   echo && printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' - && echo $1
 }
 
-# ---------------------------------------------
 # for platform independence
-# ---------------------------------------------
-REALPATH="realpath"
+READLINK_BIN="readlink -f"
+OM_BIN=om-linux
+JQ_BIN=jq-linux64
 if [ "$(uname -s)" == "Darwin" ]; then
-  REALPATH="readlink -f"
+  READLINK_BIN="realpath"
+  OM_BIN=om-darwin
+  JQ_BIN=jq-osx-amd64
 fi
-SCRIPTDIR=$(dirname $(${REALPATH} $0))
 
-OM=${SCRIPTDIR}/om-linux
-JQ=${SCRIPTDIR}/jq-linux64
-if [ "$(uname -s)" == "Darwin" ]; then
-  OM=${SCRIPTDIR}/om-darwin
-  JQ=${SCRIPTDIR}/jq-osx-amd64
-fi
-# ---------------------------------------------
+SCRIPTDIR=$(dirname $(${READLINK_BIN} $0))
+OM=${SCRIPTDIR}/${OM_BIN}
+JQ=${SCRIPTDIR}/${JQ_BIN}
 
 printline "Operation starting"
 echo
@@ -31,16 +28,18 @@ printline "Log will be created here: ${LOGFILE}"
 echo
 mkdir -p ${LOGDIR}
 
+printline "Acquiring BOSH credentials from Ops Manager"
 BOSH_AUTHENTICATED=$( \
   ${OM} -k -t ${OPSMAN_URL} -u ${OPSMAN_USER} -p ${OPSMAN_PASSWD} \
     curl -s -p /api/v0/deployed/director/credentials/bosh_commandline_credentials | \
   ${JQ} ".credential" --raw-output \
 )
 
+printline "Discovering deployments"
 mkdir -p ${LOGDIR}
 for DEPLOYMENT in $(eval ${BOSH_AUTHENTICATED} deployments | awk '{print $1}' | grep -v '\/'); do
   printline "processing $DEPLOYMENT"
-  eval ${BOSH_AUTHENTICATED} -d ${DEPLOYMENT} stop --hard --non-interactive 2>&1 | tee -a ${LOGFILE}
+  # eval ${BOSH_AUTHENTICATED} -d ${DEPLOYMENT} stop --hard --non-interactive 2>&1 | tee -a ${LOGFILE}
 done
 
 printline "Operation complete"
